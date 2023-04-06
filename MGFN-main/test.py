0caf2addd2cd715f7e200fb6416f505ee5b37c6e
marctimjen <79468558+marctimjen@users.noreply.github.com>
2023-04-06 16:03:12 +0200

    now try our

--- MGFN-main/test.py
+++ MGFN-main/test.py
@@ -64,7 +64,7 @@ if __name__ == '__main__':
         elif args.user == "cluster":
             params["save_dir"] = ""  # where to save results + model
             params["rgb_list"] = ""
-            params["gt"] = "/home/cv05f23/data/UCF/test_gt/gt-ucf.npy"
+            params["gt"] = "/home/cv05f23/data/UCF/test_gt/gt-ucf_our.npy"
             params["test_rgb_list"] = "/home/cv05f23/data/UCF/lists/ucf-i3d-test.list"
             params["pretrained_path"] = "/home/cv05f23/data/UCF/lists/UCF_pretrained/mgfn_ucf.pkl"
             return ""  # path where to wave files
