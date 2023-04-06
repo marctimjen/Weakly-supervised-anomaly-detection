d5ee6cac625cb70a007a3481ccda4e51a787ef18
marctimjen <79468558+marctimjen@users.noreply.github.com>
2023-04-06 11:09:10 +0200

    lets move some files

--- MGFN-main/create_val_split/move_files.py
+++ MGFN-main/create_val_split/move_files.py
@@ -1,15 +1,23 @@
 import shutil
 
-path = rf"C:\Users\Marc\Documents\GitHub\8 semester\Weakly-supervised-anomaly-detection\MGFN-main\UCF_list\\"
+# path = rf"C:\Users\Marc\Documents\GitHub\8 semester\Weakly-supervised-anomaly-detection\MGFN-main\UCF_list\\"
+path = rf"/home/cv05f23/git/Weakly-supervised-anomaly-detection/MGFN-main/UCF_list/"
 
 with open(path + rf"ucf-i3d-train.list", 'r') as f:  # use a context manager to safely opening and closing files
     train = [line.strip() for line in f.readlines()]  # use strip to get rid of the \n at the end of string
 
+with open(path + rf"ucf-i3d-val.list", 'r') as f:  # use a context manager to safely opening and closing files
+    val = [line.strip() for line in f.readlines()]  # use strip to get rid of the \n at the end of string
+
 old_path = "/home/cv05f23/data/train"
 new_path = "/home/cv05f23/data/UCF/train"
 for i in train:
-    shutil.move(i, )
+    shutil.move(i, new_path + i[len(old_path):])
 
 with open(path + rf"ucf-i3d-val.list", 'r') as f:  # use a context manager to safely opening and closing files
     val = [line.strip() for line in f.readlines()]  # use strip to get rid of the \n at the end of string
 
+old_path = "/home/cv05f23/data/train"
+new_path = "/home/cv05f23/data/UCF/val"
+for i in val:
+    shutil.move(i, new_path + i[len(old_path):])
