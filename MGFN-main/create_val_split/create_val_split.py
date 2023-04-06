b6b242c366f869d3c9157a74e02a9d19404645b1
marctimjen <79468558+marctimjen@users.noreply.github.com>
2023-04-06 10:42:06 +0200

    update to val split

--- MGFN-main/create_val_split/create_val_split.py
+++ MGFN-main/create_val_split/create_val_split.py
@@ -1,7 +1,7 @@
 import numpy as np
 import re
 
-path = rf"/home/cv05f23/lists/"
+path = rf"/home/cv05f23/data/UCF/lists"
 
 with open(path + rf"ucf-i3d.list", 'r') as f:  # use a context manager to safely opening and closing files
     ls = [line.strip() for line in f.readlines()]  # use strip to get rid of the \n at the end of string
@@ -11,7 +11,7 @@ vals = dict()
 
 for i in ls:
     try:
-        gr = re.match(r"/home/cv05f23/data/UCF/([a-zA-Z_]+)\d.+", i).group(1)
+        gr = re.match(r"/home/cv05f23/data/train/([a-zA-Z_]+)\d.+", i).group(1)
     except TypeError:
         print("")
 
@@ -40,14 +40,17 @@ for key in vals.keys():
     validation_set.extend([vals[key][i] for i in random_nr[:anomaly_nr[key]]])
     train_set.extend([vals[key][i] for i in random_nr[anomaly_nr[key]:]])
 
-
+old_path = "/home/cv05f23/data/train"
+new_path = "/home/cv05f23/data/UCF/train"
 with open(path + rf"ucf-i3d-train.list", 'w+') as f:  # use a context manager to safely opening and closing files
     for j in train_set:
-        f.write(f"{j}\n")  # https://www.scaler.com/topics/python-write-list-to-file/
+        final_path = new_path + j[len(old_path):]
+        f.write(f"{final_path}\n")  # https://www.scaler.com/topics/python-write-list-to-file/
 
 with open(path + rf"ucf-i3d-val.list", 'w+') as f:  # use a context manager to safely opening and closing files
     for j in validation_set:
-        f.write(f"{j}\n")  # https://www.scaler.com/topics/python-write-list-to-file/
+        final_path = new_path + j[len(old_path):]
+        f.write(f"{final_path}\n")  # https://www.scaler.com/topics/python-write-list-to-file/
 
 
 
