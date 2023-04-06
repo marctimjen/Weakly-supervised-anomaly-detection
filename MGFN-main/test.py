7c844b7c4b56c890059f744fa04de6ded57c60f6
marctimjen <79468558+marctimjen@users.noreply.github.com>
2023-04-06 15:51:21 +0200

    Update test.py

--- MGFN-main/test.py
+++ MGFN-main/test.py
@@ -84,8 +84,10 @@ if __name__ == '__main__':
     test_loader = DataLoader(Dataset(rgb_list=param["test_rgb_list"], datasetname="UCF", modality="RGB", seg_length=32,
                                      test_mode=True),
                              batch_size=1, shuffle=False, num_workers=0, pin_memory=False)
+    print(params["pretrained_path"])
+    model = model.to("cpu")
+    model_dict = model.load_state_dict({k.replace('module.', ''): v for k, v in torch.load(params["pretrained_path"], map_location="cpu").items()})
     model = model.to(device)
-    model_dict = model.load_state_dict({k.replace('module.', ''): v for k, v in torch.load(params["pretrained_path"], map_location=device).items()})
     auc = test(test_loader, model, param, device)
 
 
