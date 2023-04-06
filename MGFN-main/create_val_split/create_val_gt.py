bf8a82c2a6e592506fc01a6a10a99d305d239481
marctimjen <79468558+marctimjen@users.noreply.github.com>
2023-04-06 11:27:11 +0200

    add counter

--- MGFN-main/create_val_split/create_val_gt.py
+++ MGFN-main/create_val_split/create_val_gt.py
@@ -4,6 +4,10 @@ path = rf"/home/cv05f23/git/Weakly-supervised-anomaly-detection/MGFN-main/UCF_li
 with open(path + rf"ucf-i3d-val.list", 'r') as f:  # use a context manager to safely opening and closing files
     val = [line.strip() for line in f.readlines()]  # use strip to get rid of the \n at the end of string
 
+j = 0
 for i in val:
     print(i)
     print(np.load(i).shape)
+    j += 1
+
+print(j)
