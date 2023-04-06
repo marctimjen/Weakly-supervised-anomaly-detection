b3b6fb472280b79522f815c7112fc31971b1cfa6
marctimjen <79468558+marctimjen@users.noreply.github.com>
2023-04-06 11:26:51 +0200

    test the dir

---
+++ MGFN-main/create_val_split/create_val_gt.py
@@ -0,0 +1,9 @@
+import numpy as np
+path = rf"/home/cv05f23/git/Weakly-supervised-anomaly-detection/MGFN-main/UCF_list/"
+
+with open(path + rf"ucf-i3d-val.list", 'r') as f:  # use a context manager to safely opening and closing files
+    val = [line.strip() for line in f.readlines()]  # use strip to get rid of the \n at the end of string
+
+for i in val:
+    print(i)
+    print(np.load(i).shape)
