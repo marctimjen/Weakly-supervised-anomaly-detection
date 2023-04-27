91dc5939172df4a586c1bf3110f3e08848639d02
marctimjen <79468558+marctimjen@users.noreply.github.com>
2023-04-27 07:59:41 +0200

    process master

--- process_master/process_master.py
+++ process_master/process_master.py
@@ -2,40 +2,38 @@ import time
 import subprocess
 
 
-flag = False
+i = 0
+
+# py_path = "/home/marc/anaconda3/envs/pyt/bin/python3"
+# path = "/home/marc/Documents/GitHub/8semester/Weakly-supervised-anomaly-detection/process_master/process.txt"
+
+py_path = "/home/cv05f23/.conda/envs/weak/bin/python3"
+path = "/home/cv05f23/git/Weakly-supervised-anomaly-detection/process_master/process.txt"
+
 while True:
-    path = "/home/marc/Documents/GitHub/8semester/Weakly-supervised-anomaly-detection/process_master/process.txt"
     with open(path, "r") as f:
         file = []
         for line in f:
             file.append(line.strip("\n"))
 
-    # print(file)
+    file = [[py_path] + i.split(" ") for i in file]
 
-    if not flag:
-        command = ["/home/marc/anaconda3/envs/pyt/bin/python3", "/home/marc/Documents/GitHub/8semester/Weakly-supervised-anomaly-detection/MGFNmain/cheat/cheat_test.py", "-u", "marc", "-p", "params_cheat"] # "/home/marc/Documents/GitHub/8semester/Weakly-supervised-anomaly-detection/process_master/long.py"]
+    if i >= len(file):
+        print("sleeping at iteration:", i)
+        time.sleep(60)
+        continue
+    else:
+        print("At iteration:", i)
 
-        p = subprocess.Popen(command, stdout=subprocess.PIPE)
-        # text = p.communicate()
-        (output, err) = p.communicate()
+    command = file[i]
 
-    flag = True
-
-
+    p = subprocess.Popen(command, stdout=subprocess.PIPE)
+    # text = p.communicate()
+    (output, err) = p.communicate()
 
     # This makes the wait possible
     p_status = p.wait()
 
-    # This will give you the output of the command being executed
-    print(output)
-
-    print("2")
-
-    # print(p)
-    # print(text)
-    # print(type(text))
-
-
-
     time.sleep(1)
+    i += 1
 
