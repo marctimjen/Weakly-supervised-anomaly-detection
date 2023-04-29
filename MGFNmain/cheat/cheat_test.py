69b8ab61a50e56c4954996578d5e84e4eb693d7b
marctimjen <79468558+marctimjen@users.noreply.github.com>
2023-04-29 15:51:45 +0200

    update to run 8

--- MGFNmain/cheat/cheat_test.py
+++ MGFNmain/cheat/cheat_test.py
@@ -82,12 +82,12 @@ if __name__ == '__main__':
     run = neptune.init_run(
         project="AAM/mgfn",
         api_token=token,
-        with_id="MGFN-7"
+        with_id="MGFN-8"
     )
 
     for i in range(param["max_epoch"]):
         # param["pretrained_path"] = f"/home/marc/Documents/sandbox/mgfn/nept_id_AN-110/mgfn{i}-i3d.pkl"
-        param["pretrained_path"] = f"/home/cv05f23/data/UCF/results/mgfn/nept_id_MGFN-7/mgfn{i}-i3d.pkl"
+        param["pretrained_path"] = f"/home/cv05f23/data/UCF/results/mgfn/nept_id_MGFN-8/mgfn{i}-i3d.pkl"
         model = Model(dropout=0.0, attention_dropout=0.0, dropout_rate=0.0)
 
         test_loader = DataLoader(Dataset(rgb_list=param["test_rgb_list"], datasetname="UCF", modality="RGB", seg_length=32,
