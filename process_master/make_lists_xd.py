8033522aaa1c9073708d512edec993b838afaeab
marctimjen <79468558+marctimjen@users.noreply.github.com>
2023-05-02 07:54:47 +0200

    Make lists for the xd violence data

---
+++ process_master/make_lists_xd.py
@@ -0,0 +1,41 @@
+import os
+# https://github.com/Roc-Ng/XDVioDet/blob/master/list/make_list.py
+
+path = "/home/cv05f23/data/XD/i3d-features/RGB"
+files = os.listdir(path)
+name = '/home/cv05f23/data/XD/i3d-features/lists/rgb.list'
+
+with open(name, 'w+') as f:  ## the name of feature list
+    for file in files:
+        newline = file+'\n'
+        f.write(newline)
+
+
+path = "/home/cv05f23/data/XD/i3d-features/RGBTest"
+files = os.listdir(path)
+name = '/home/cv05f23/data/XD/i3d-features/lists/rgbtest.list'
+
+with open(name, 'w+') as f:  ## the name of feature list
+    for file in files:
+        newline = file+'\n'
+        f.write(newline)
+
+
+path = "/home/cv05f23/data/XD/i3d-features/FlowTest"
+files = os.listdir(path)
+name = '/home/cv05f23/data/XD/i3d-features/lists/flowtest.list'
+
+with open(name, 'w+') as f:  ## the name of feature list
+    for file in files:
+        newline = file+'\n'
+        f.write(newline)
+
+
+path = "/home/cv05f23/data/XD/i3d-features/Flow"
+files = os.listdir(path)
+name = '/home/cv05f23/data/XD/i3d-features/lists/flow.list'
+
+with open(name, 'w+') as f:  ## the name of feature list
+    for file in files:
+        newline = file+'\n'
+        f.write(newline)
