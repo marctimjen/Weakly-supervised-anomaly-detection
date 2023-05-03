a3a1dcefb073114a9dd52f873cef3e09644948e6
marctimjen <79468558+marctimjen@users.noreply.github.com>
2023-05-03 16:56:54 +0200

    Update MGFN_cheat_xd.py

--- MGFNmain/MGFN_cheat_xd.py
+++ MGFNmain/MGFN_cheat_xd.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
 
     token = os.getenv('NEPTUNE_API_TOKEN')
     run = neptune.init_run(
-        project="AAM/anomaly",
+        project="AAM/mgfnxd",
         api_token=token,
     )
     run_id = run["sys/id"].fetch()
