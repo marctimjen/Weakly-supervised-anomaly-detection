10036d30cdedfd2fc85ce2df27590cffd0b144b9
Anastasia Drozdova <avdrozdova3@yandex.ru>
2023-05-13 13:25:46 +0200

    fix

--- S3R/tools/trainval_anomaly_detector.py
+++ S3R/tools/trainval_anomaly_detector.py
@@ -166,13 +166,14 @@ def main():
             #          "recall": recall,
             #          "average_precision": ap}
 
+
             score = inference(test_loader, model, args, device, rec_auc_only=False)
 
             # >> Performance
             title = [['Dataset', 'Method', 'Feature', 'REC AUC (%)', 'PR AUC (%)', 'F1', 'F1 macro', 'accuracy', 'precision', 'recall', 'average precision']]
-            score = [[args.dataset, args.model_name.upper(), args.backbone.upper(), f'{score['rec_auc']*100.:.3f}', f'{score['pr_auc']*100.:.3f}', f'{score['f1']*100.:.3f}', f'{score['f1_macro']*100.:.3f}', f'{score['accuracy']*100.:.3f}', f'{score['precision']*100.:.3f}', f'{score['recall']*100.:.3f}', f'{score['average_precision']*100.:.3f}']]
+            scores = [[args.dataset, args.model_name.upper(), args.backbone.upper(), f'{score['rec_auc']*100.:.3f}', f'{score['pr_auc']*100.:.3f}', f'{score['f1']*100.:.3f}', f'{score['f1_macro']*100.:.3f}', f'{score['accuracy']*100.:.3f}', f'{score['precision']*100.:.3f}', f'{score['recall']*100.:.3f}', f'{score['average_precision']*100.:.3f}']]
 
-            table = AsciiTable(title + score, ' Performance on {} '.format(args.dataset))
+            table = AsciiTable(title + scores, ' Performance on {} '.format(args.dataset))
             for i in range(len(title[0])):
                 table.justify_columns[i] = 'center'
             logger.info('Summary Result on {} metric\n{}'.format('AUC', table.table))
